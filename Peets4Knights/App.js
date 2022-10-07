9c1a45b27ff0781a631b4f32d1fc96ebe3494209
Ben Tanis <bentanis@Thor.local>
2022-10-06 23:54:30 -0400

    Settings Page V1.0

--- Peets4Knights/App.js
+++ Peets4Knights/App.js
@@ -2,12 +2,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
 
 import HomeScreen from "./screens/home";
 import LoginScreen from "./screens/login";
 import RegScreen from "./screens/register";
 import MenuSelectionScreen from "./screens/menuSelection";
 import CustomizeScreen from './screens/customize';
+import SettingsScreen from './screens/settings';
+import Header from './shared/header';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,12 +18,26 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} 
+        options={({ navigation })=> ({headerRight: () => (
+          <Header navigation={navigation}/>
+        )})} />
+        
+        <Stack.Screen name="login" component={LoginScreen} 
+        options={({ navigation })=> ({headerRight: () => (
+          <Header navigation={navigation}/>
+        )})} />
+
         <Stack.Screen name="Register" component={RegScreen} />
-        <Stack.Screen name="menuSelection"
-          component={MenuSelectionScreen} />
+        
+        <Stack.Screen name="menuSelection" 
+          component={MenuSelectionScreen} 
+          options={({ navigation })=> ({headerRight: () => (
+          <Header navigation={navigation}/>
+        )})} />
+        
         <Stack.Screen name="customize" component={CustomizeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ Peets4Knights/shared/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, Image } from 'react-native';
+import { globalStyles, settings } from '../styles/globalStyles';
+import About from "../screens/settings";
+
+export default function Header({ navigation }) {
+    return (
+        <View>
+            <TouchableOpacity style={settings.cornerbutton} onPress={() => navigation.navigate('Settings')}>
+                <Image style={settings.cornerbutton} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png'}}></Image>
+            </TouchableOpacity>
+        </View>
+    );
+};
