02d3b0e1be39ff9d99e4a2187e63affcd98e5584
rjswn97 <gunjudavid@naver.com>
2022-11-18 01:56:15 -0500

    customization option button fixed

--- Peets4Knights/screens/customize.js
+++ Peets4Knights/screens/customize.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Icon, Text, TouchableOpacity, View, Image } from 'react-native';
+import { MultipleSelectList } from 'react-native-dropdown-select-list'
+
 import tw from 'tailwind-react-native-classnames'
 
 import HeaderTabs from '../components/Size';
@@ -7,6 +9,7 @@ import { customizeStyle, globalStyles } from '../styles/globalStyles';
 import { menus } from '../database/menuDataworking';
 
 const fontStyles = ["normal", "italic"];
+
 // create customization page
 export default function CustomizeScreen({ route, navigation }) {
 
@@ -14,6 +17,16 @@ export default function CustomizeScreen({ route, navigation }) {
     console.log(text)
     console.log(image)
     const [activeTab, setActiveTab] = React.useState("Delivery");
+    const [selected, setSelected] = React.useState([]);
+  
+    const custom = [
+        {key:'1', value:'Add Sugar'},
+        {key:'2', value:'Sugar Free'},
+        {key:'3', value:'Add Whipped Cream'},
+        {key:'4', value:'Add shot'},
+        {key:'5', value:'Decaf'},
+        {key:'6', value:'Add Syrup'},
+    ]
 
     console.log(addon)
     return (
@@ -26,152 +39,46 @@ export default function CustomizeScreen({ route, navigation }) {
             </View>
 
             <HeaderTabs/>
-
-            {/* <View style={tw`flex-row bg-white justify-evenly py-2 border-t border-gray-100`}>
-                <TouchableOpacity
-                    style={tw`flex flex-row bg-black w-24 px-10 py-3 rounded-full text-center`}
-                >
-                    <Text style={tw`text-white text-center`}>S</Text>
-
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={tw`flex flex-row bg-white w-24 px-10 py-3 rounded-full text-center`}
-                >
-                    <Text style={tw`text-black text-center`}>M</Text>
-
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={tw`flex flex-row bg-white w-24 px-10 py-3 rounded-full text-center`}
-                >
-                    <Text style={tw`text-black text-center`}>L</Text>
-
-                </TouchableOpacity>
-            </View> */}
-
-            <View style={customizeStyle.addcontainer}>
+  
+            {/* <View style={customizeStyle.addcontainer}>
                 <View style={customizeStyle.additem}>
-                    <Text style={customizeStyle.customText}>Sugar Free</Text>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={() => (handleAddTask())}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
+                    <Text style={customizeStyle.customText}>sugar</Text>
                 </View>
             </View>
-            <View style={customizeStyle.addcontainer}>
-                <View style={customizeStyle.additem}>
-                    <Text style={customizeStyle.customText}>Sugar Light</Text>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={() => handleAddTask()}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            
             <View style={customizeStyle.addcontainer}>
                 <View style={customizeStyle.additem}>
                     <Text style={customizeStyle.customText}>Whipped Cream</Text>
                 </View>
-                <View>
-                    <TouchableOpacity onPress={() => handleAddTask()}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
+
+            </View>
+
+            <View style={customizeStyle.addcontainer}>
+                <View style={customizeStyle.additem}>
+                    <Text style={customizeStyle.customText}>Syrup</Text>
                 </View>
             </View>
 
             <View style={customizeStyle.addcontainer}>
                 <View style={customizeStyle.additem}>
-                    <Text style={customizeStyle.customText}>Add Syrup</Text>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={() => handleAddTask()}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
+                    <Text style={customizeStyle.customText}>Shots</Text>
                 </View>
             </View>
-            <View style={customizeStyle.addcontainer}>
-                <View style={customizeStyle.additem}>
-                    <Text style={customizeStyle.customText}>Add Sauce</Text>
-                </View>
-                <View>
-                    <TouchableOpacity onPress={() => handleAddTask()}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
+
             <View style={customizeStyle.addcontainer}>
                 <View style={customizeStyle.additem}>
                     <Text style={customizeStyle.customText}>Decaf</Text>
                 </View>
-                <View>
-                    <TouchableOpacity onPress={() => handleAddTask()}>
-                        <View style={customizeStyle.amount}>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>-</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>0</Text>
-                            </View>
-                            <View style={customizeStyle.addWrapper}>
-                                <Text style={customizeStyle.addText}>+</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            </View> */}
+            <MultipleSelectList 
+                setSelected={(val) => setSelected(val)} 
+                data={custom} 
+                save="value"
+                label="Categories"
+            />
 
             <View style={customizeStyle.itemTextBlock}>
-                <Text style={customizeStyle.priceText}>Total price: </Text>
+                <Text style={customizeStyle.priceText}>Total price: $4.29</Text>
             </View>
 
 
